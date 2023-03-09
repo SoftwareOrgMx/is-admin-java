f65e7175f8a2c87937b2ba3fdda540003ad7a5e5
https://software.org.mx <126974543+SoftwareOrgMx@users.noreply.github.com>
2023-03-08 21:30:22 -0600

    Initial

---
+++ main.java
@@ -0,0 +1,32 @@
+import java.io.PrintStream;
+import java.util.prefs.Preferences;
+
+/**
+ *
+ * @author SoftwareOrgMX
+ */
+public class Mains {
+
+    public static void main(String[] args) {
+        System.out.println("Is Admin: " + isAdmin());
+    }
+
+    public static boolean isAdmin() {
+
+        Preferences prefs = Preferences.systemRoot();
+        PrintStream systemErr = System.err;
+        synchronized (systemErr) {    // better synchroize to avoid problems with other threads that access System.err
+            System.setErr(null);
+            try {
+                prefs.put("foo", "bar"); // SecurityException on Windows
+                prefs.remove("foo");
+                prefs.flush(); // BackingStoreException on Linux
+                return true;
+            } catch (Exception e) {
+                return false;
+            } finally {
+                System.setErr(systemErr);
+            }
+        }
+    }
+}
